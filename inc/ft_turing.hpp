c24e185dd1dde4194c16486f286bec565f7cd183
Mohamed Amine Kariouh <med79801@gmail.com>
2026-04-20 10:52:54 +0200

    added

---
+++ inc/ft_turing.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_turing.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mokariou <mokariou>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/14 14:01:26 by mokariou          #+#    #+#             */
+/*   Updated: 2026/04/20 10:35:44 by mokariou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_TURING_HPP
+# define FT_TURING_HPP
+# include <iostream>
+# include <string>
+# include <vector>
+# include <map>
+# include <set>
+# include <algorithm>
+# include <stdexcept>
+# include <fstream>
+# include <sstream>
+# include <cctype>
+# include <cstdlib>
+# include <ctime>
+# include <cmath>
+# include <limits>
+# include <iomanip>
+# include <memory>
+# include <utility>
+# include <functional>
+# include <type_traits>
+# include <iterator>
+# include <numeric>
+#include <algorithm>
+#include <filesystem>
+#include <string>
+#include <OcamL>
+#include <nlohmann/json.hpp>
+
+struct Machine {
+    std::string name;
+    std::vector<std::string> alphabet;
+    std::string blank;
+    std::vector<std::string> states;
+    std::string initial;
+    std::vector<std::string> finals;
+    std::map<std::string, std::vector<Transition>> transitions;
+};
+struct Transition {
+    std::string read;
+    std::string to_state;
+    std::string write;
+    std::string action;
+};
+class TuringMachine {
+    public:
+        TuringMachine();
+        ~TuringMachine();
+
+}; 
